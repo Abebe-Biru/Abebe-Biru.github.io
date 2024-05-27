4fc4648b2651281cfd56e52a90c31af78dda6704
abesoboka <abesoboka@gmail.com>
2024-05-27 00:42:46 +0000

    Reverted to the previous

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-(function() {
-    emailjs.init("YOUR_USER_ID"); // Replace with your EmailJS user ID
-})();
-
 function showProjectDetails(projectName) {
     Swal.fire({
         title: projectName,
@@ -17,25 +13,15 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
-    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', {
-        from_name: name,
-        from_email: email,
-        message: message
-    })
-    .then((response) => {
-        Swal.fire({
-            title: 'Thank you!',
-            text: 'Your message has been sent.',
-            icon: 'success',
-            confirmButtonText: 'Close'
-        });
-        document.getElementById('contactForm').reset();
-    }, (error) => {
-        Swal.fire({
-            title: 'Oops...',
-            text: 'Something went wrong. Please try again later.',
-            icon: 'error',
-            confirmButtonText: 'Close'
-        });
+    // Here you can add code to handle form submission, like sending an email
+
+    Swal.fire({
+        title: 'Thank you!',
+        text: 'Your message has been sent.',
+        icon: 'success',
+        confirmButtonText: 'Close'
     });
+
+    // Clear the form
+    document.getElementById('contactForm').reset();
 });
